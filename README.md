558a5618d5d36a5bb144dd78397f7b6d2f90538b
Vitaliy Kachan <kachan.vitaliy12@gmail.com>
2026-01-29 12:21:10 +0200

    readme, local testing update

---
+++ README.md
@@ -0,0 +1,13 @@
+This is a web-component that verifies face liveliness
+
+Utilizes face-api.js for face recognition models support
+
+Component inputs:
+* token - token for api
+* model-url - url for face recognition models, for local test - "/models"
+
+Component outputs:
+* on-analysis-complete - output after varification complete
+* on-error - output after error occurs
+* on-user-cancel - output after user cancel verification
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,22 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    // This section allows the browser to access the /dist folder
+    // while you are running 'npm run dev'
+    server: {
+        fs: {
+            allow: ['.']
+        }
+    },
     build: {
         lib: {
             entry: 'src/main.ts',
-            name: 'FaceValidationWC', // global variable name
+            name: 'FaceValidationWC', // global variable name for IIFE
             formats: ['iife'],
             fileName: () => 'face-validation.iife.js',
         },
         rollupOptions: {
-            // DO NOT externalize face-api.js for IIFE
+            // DO NOT externalize face-api.js for IIFE to ensure it's plug-and-play
             external: [],
         },
         emptyOutDir: true,
